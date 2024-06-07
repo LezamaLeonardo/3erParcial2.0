26fb7ab1c3f3e0a385ed541b4cd1f47ece6ec5b6
LezamaLeonardo <109265347+LezamaLeonardo@users.noreply.github.com>
2024-06-06 21:16:58 -0600

    lee 10 numeros y regresa el numero mayor, el numero menor y la suma total de los numeros

--- activ24.cpp
+++ activ24.cpp
@@ -1,1 +1,33 @@
-24
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, me, ma;
+  int i;
+  int su=0;
+  me=0;
+  ma=0;
+
+cout<<"dime 10 numeros";
+cin>>n;
+me=n;
+ma=n;
+su+=n;
+
+for(int i=1;i<10;i++){
+cin>>n;
+su+=n;
+
+if(nu<me){;
+me=n;
+}
+if(n>ma){
+ma=n;
+}
+}
+cout<<"el numero mayor es"<<ma<<endl;
+cout<<"el numero menor es"<<me<<endl;
+cout<<"la suma de los numeros es"<<su<<endl;
+
+return 0;
+}
