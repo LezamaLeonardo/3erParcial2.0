9cf8a0973d1b587e6a7e5c6d673c052efe6a3673
LezamaLeonardo <109265347+LezamaLeonardo@users.noreply.github.com>
2024-06-06 21:37:31 -0600

    usuario=root contrasena=123

--- activ26.cpp
+++ activ26.cpp
@@ -1,1 +1,30 @@
-26
+int main() {
+    std::string usuario;
+    std::string contra;
+    int intentos = 3;
+
+    while (intentos > 0) {
+        std::cout << "Nombre de usuario: ";
+        std::cin >> usuario;
+        std::cout << "Contraseña: ";
+        std::cin >> contra;
+
+        if (usuario == "root" && contra == "123") {
+            std::cout << "Bienvenido al sistema" << std::endl;
+            break;
+        } else {
+            std::cout << "Nombre o contraseña incorrectos. Intentos restantes: " << --intentos << std::endl;
+        }
+
+        if (intentos == 0) {
+            std::cout << "Se ha alcanzado el número máximo de intentos" << std::endl;
+            break;
+        }
+    }
+
+    if (intentos > 0) {
+       
+    }
+
+    return 0;
+}
