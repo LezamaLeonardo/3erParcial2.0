3d1e2b7fd7f7ae06845ce903553434bb69e1c7ed
LezamaLeonardo <109265347+LezamaLeonardo@users.noreply.github.com>
2024-06-06 21:12:39 -0600

    eleva un numero x a una potencia x requiere de la librería math

---
+++ activ23.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int main(){
+
+int n;
+int p;
+
+cout<<"dime el numero que quieres elevar";
+cin>>n;
+
+cout<<"dime la potencia";
+cin>>p;
+
+float resultado=pow(n,p);
+
+cout<<numero<< " elevado a la potencia de" <<potencia<< "es" <<resultado<<endl;
+return 0;
+}
